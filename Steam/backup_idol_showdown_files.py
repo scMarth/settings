d69707c18e0a865cd6183c9f856452dd5169d29f
scMarth <vlantaca@gmail.com>
2023-06-04 22:14:57 -0700

    add idol showdown save backup script

---
+++ Steam/backup_idol_showdown_files.py
@@ -0,0 +1,54 @@
+import shutil, os, sys
+
+workspace = os.getcwd()
+
+# https://store.steampowered.com/account/remotestorage
+
+src = r'C:\Users\<user>\AppData\LocalLow\BestoGameStudios\Idol Showdown'  # Replace with the source directory path
+dst = workspace + r'\Idol Showdown'  # Replace with the destination directory path
+
+dst_num = -1
+
+for folder in os.listdir(workspace):
+    folder_path = os.path.join(workspace, folder)
+
+    if not os.path.isdir(folder_path):
+        continue
+    else:
+        if 'Idol Showdown' not in folder_path:
+            continue
+
+    print('folder_path: {}'.format(folder_path))
+    number = folder_path.split('Showdown')[1]
+    if number:
+        number = int(number)
+
+    if not number:
+        number = 0
+    number += 1
+
+    if number > dst_num:
+        dst_num = number
+
+dst += str(dst_num)
+
+print('src: {}'.format(src))
+print('dst: {}'.format(dst))
+
+if os.path.exists(dst):
+    shutil.rmtree(dst)
+
+# Create the destination directory if it doesn't exist
+if not os.path.exists(dst):
+    os.makedirs(dst)
+
+# Copy the contents of the source directory to the destination directory
+for item in os.listdir(src):
+    src_item = os.path.join(src, item)
+    dst_item = os.path.join(dst, item)
+    if os.path.isdir(src_item):
+        shutil.copytree(src_item, dst_item)
+    else:
+        shutil.copy2(src_item, dst_item)
+
+print("Copy complete!")
